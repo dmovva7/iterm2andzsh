a1ee5a5111f6dfdebb83de5817d2bf8552cefabb
Dinesh Movva <dineshkumar.movva7@gmail.com>
2021-12-24 10:54:03 -0600

    Update complete_script.sh

--- complete_script.sh
+++ complete_script.sh
@@ -23,3 +23,33 @@ cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complet
 
 # reset the terminal
 source ~/.zshrc
+
+
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+   26  pwd
+   27  /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+   28  vi .zshrc
+   29  source .zshrc
+   30  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+   31  git clone https://github.com/powerline/fonts.git --depth=1\ncd fonts\n./install.sh\ncd ..\nrm -rf fonts
+   32  cd fonts
+   33  pwd
+   34  ls -lrth
+   35  git clone https://github.com/powerline/fonts.git --depth=1
+   36  cd fonts
+   37  ./install.sh
+   38  cd ..
+   39  rm -rf fonts
+   40  https://github.com/mbadolato/iTerm2-Color-Schemes.git
+   41  git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git
+   42  pwd
+   43  clear
+   44  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+   45  vi .zshrc
+   46  . .zshrc
+   47  . ~/.zshrc
+   48  vi .zshrc
+   49  . ~/.zshrc
+   50  ll
+   51  c
+   52  vi .zshrc
